ed7330bb0d04a4f9f994b0182543861ea03554d6
shenry45 <shawn45henry@gmail.com>
2019-06-27 20:03:10 -0700

    fix props.history bug from nested comp

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import LoginForm from './LoginForm';
+import { connect } from 'react-redux';
 
-const Login = () => {
-  return (
-    <div className="login">
-      <LoginForm />
-    </div>
-  )
+import { logIn } from '../actions/logIn';
+
+class Login extends React.Component {
+  state = {
+      username: '',
+      password: ''
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleLoggingIn = e => {
+    e.preventDefault();
+
+    this.props
+      .logIn(this.state)
+      .then(() => this.props.history.push("/friends"))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleLoggingIn}>
+        <input
+          name='username'
+          value={this.props.username}
+          onChange={this.handleChanges}
+        ></input>
+        <input
+          name='password'
+          value={this.props.password}
+          onChange={this.handleChanges}
+        ></input>
+        <button
+          type='submit'
+          onClick={this.handleLoggingIn}
+        >Login</button> 
+      </form>
+    )
+  }
 }
 
-export default Login;
+export default connect(null, { logIn })(Login);
